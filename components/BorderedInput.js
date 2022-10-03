51db3937972d97a59b261418b5724288d5af62c6
oneway1020 <hangeul6851@gmail.com>
2022-10-04 03:45:35 +0900

    first upload

---
+++ components/BorderedInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+
+function BorderedInput({hasMarginBottom, ...rest}, ref) {
+  return (
+    <TextInput
+      style={[styles.input, hasMarginBottom && styles.margin]}
+      ref={ref}
+      {...rest}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: '#bdbdbd',
+    borderWidth: 1,
+    paddingHorizontal: 16,
+    height: 48,
+    borderRadius: 4,
+    backgroundColor: 'white',
+  },
+  margin: {
+    marginBottom: 16,
+  },
+});
+
+export default React.forwardRef(BorderedInput);
+// const BorderedInput = React.forwrdRef(({hasMarginBottom, ...rest}, ref) => {
+//      return(...)
+// });  이런식으로 사용할 수도 있음
