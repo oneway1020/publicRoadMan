ebae76364b35f923478d9df28477b9e66346d807
oneway1020 <hangeul6851@gmail.com>
2022-10-04 21:40:32 +0900

    9.3.3 키보드 이벤트 리스너까지

--- screens/MainTab.js
+++ screens/MainTab.js
@@ -1,31 +1,52 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {useUserContext} from '../contexts/UserContext';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeStack from './HomeStack';
+import MyProfileStack from './MyProfileStack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import CameraButton from '../components/CameraButton';
+import {StyleSheet, View} from 'react-native';
+
+const Tab = createBottomTabNavigator();
 
 function MainTab() {
-  const {user} = useUserContext();
   return (
-    <View style={styles.block}>
-      {user.photoURL && (
-        <Image
-          source={{uri: user.photoURL}}
-          style={{width: 128, height: 128, marginBottom: 16}}
-          resizeMode="cover"
-        />
-      )}
-      <Text style={styles.text}>Hello, {user.displayName}</Text>
-    </View>
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200ee',
+          }}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyProfileStack"
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="person" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+      <CameraButton />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
+    zIndex: 0,
   },
 });
 
---
+++ screens/UploadScreen.js
@@ -0,0 +1,73 @@
+import {useRoute} from '@react-navigation/native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Animated,
+  Keyboard,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
+
+function UploadScreen() {
+  const route = useRoute();
+  const {res} = route.params || {};
+  const {width} = useWindowDimensions();
+  const animation = useRef(new Animated.Value(width)).current;
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
+  useEffect(() => {
+    const didShow = Keyboard.addListener('keyboardDidShow', () =>
+      setIsKeyboardOpen(true),
+    );
+    const didHide = Keyboard.addListener('keyboardDidHide', () =>
+      setIsKeyboardOpen(false),
+    );
+    return () => {
+      didShow.remove();
+      didHide.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isKeyboardOpen ? 0 : width,
+      useNativeDriver: false,
+      duration: 150,
+      delaty: 100,
+    }).start();
+  }, [isKeyboardOpen, width, animation]);
+
+  return (
+    <View style={styles.block}>
+      <Animated.Image
+        source={{uri: res.assets[0]?.uri}}
+        style={[styles.image, {height: animation}]}
+        resizeMode="cover"
+      />
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        placeholder="이 사진에 대한 설명을 입력하세요..."
+        textAlignVertical="top"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+  image: {width: '100%'},
+  input: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    flex: 1,
+    fontSize: 16,
+  },
+});
+
+export default UploadScreen;
